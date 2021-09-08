300967d7eaef579b4bb8a0d24bfa3cae4c4384c8
lucas blommers <lucas-blommers@live.nl>
2021-09-08 12:09:32 +0200

    elif-statement

---
+++ main.py
@@ -0,0 +1,10 @@
+
+a = input("Geef een getal op.\n")
+b = input("Geef nog een getal op.\n")
+
+if(a > b):
+    max = a
+    print("A is het grootste getal: " + max)
+elif(a < b):
+    min = a
+    print("A is het kleinste getal: " + min)
